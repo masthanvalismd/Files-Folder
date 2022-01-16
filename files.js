70d50d4436c8d25c1c1dffbaa0c6e9d307a19ae5
MasthanValiSMD <masthanvalismd@gmail.com>
2022-01-16 13:28:23 +0530

    files

---
+++ files.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+console.log(process.argv) //to take the parameter in command
+
+//to read the files
+fs.readdir(`${__dirname}/${process.argv[2]}`, (err, files) => {
+    //to catch the errors
+    if (err) {
+        console.log("error occured",err)
+    } //if no errors occured it print files
+    else {
+        console.log(files);
+       
+    }
+})
